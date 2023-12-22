2ecf9b0032f64de5b0451e7e4d06e1b87ebbedfa
MelvinAguilar <00067621@uca.edu.sv>
2023-12-22 10:42:32 -0600

    :sparkles: Update public routes

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -56,6 +56,7 @@ export async function POST(req: Request) {
 
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log("Webhook body:", body);
+
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -66,11 +66,6 @@ export async function deleteUser(params: DeleteUserParams) {
       throw new Error("User not found");
     }
 
-    // get user question ids
-    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
-    //   "_id"
-    // );
-
     // delete user questions
     await Question.deleteMany({ author: user._id });
 
